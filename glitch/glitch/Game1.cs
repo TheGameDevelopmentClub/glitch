89fb525acaedf137b4bf9be797787f5ce75c6c43
Nick Boen <NickABoen@gmail.com>
2015-10-23 20:42:57 -0500

    Updates the Screen spawn position and size to a more user friendly level.

--- glitch/glitch/Game1.cs
+++ glitch/glitch/Game1.cs
@@ -11,15 +11,11 @@ namespace glitch
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Rectangle Screen;
 
         public Game1()
         {
-            Rectangle screen = new Rectangle(0, 0, 1366, 768);
-
             graphics = new GraphicsDeviceManager(this);
-            graphics.PreferredBackBufferHeight = screen.Height;
-            graphics.PreferredBackBufferWidth = screen.Width;
-            graphics.ApplyChanges();
             Content.RootDirectory = "Content";
         }
 
@@ -31,8 +27,16 @@ namespace glitch
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //Setting up the screen
+            Screen = new Rectangle(0, 0, 1366, 768);
 
+            //Spawning the game window in the upper left of the screen
+            var form = (System.Windows.Forms.Form)System.Windows.Forms.Control.FromHandle(this.Window.Handle);
+            form.Location = new System.Drawing.Point(Screen.X, Screen.Y); 
+
+            graphics.PreferredBackBufferHeight = Screen.Height;
+            graphics.PreferredBackBufferWidth = Screen.Width;
+            graphics.ApplyChanges();
             base.Initialize();
         }
 
