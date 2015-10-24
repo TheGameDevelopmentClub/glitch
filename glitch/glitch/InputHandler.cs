1f18ae755209b7c5fda6a3b3f0fc4220998e6a83
cpiggott <cpiggott@ksu.edu>
2015-10-24 01:17:19 -0500

    Chris sucks at logic at 1:15am

---
+++ glitch/glitch/InputHandler.cs
@@ -0,0 +1,85 @@
+﻿﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace glitch
+{
+    public class InputHandler
+    {
+        public static InputHandler inputHandler = null;
+
+        KeyboardState currentKeyboardState;
+        KeyboardState previousKeyboardState;
+
+        // Gamepad states used to determine button presses
+        GamePadState currentGamePadState;
+        GamePadState previousGamePadState;
+
+        public InputHandler() { }
+
+        public static InputHandler GetInstance()
+        {
+            if (inputHandler == null)
+            {
+                inputHandler = new InputHandler();
+            }
+            return inputHandler;
+        }
+
+        public void updateStates()
+        {
+            previousKeyboardState = currentKeyboardState;
+            previousGamePadState = currentGamePadState;
+
+
+            currentKeyboardState = Keyboard.GetState();
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+        }
+        public void handlePlayerInput(PlayerObject playerObject)
+        {
+
+            if (currentKeyboardState.IsKeyDown(Keys.Space) || currentGamePadState.IsButtonDown(Buttons.A))
+            {
+                if (!playerObject.IsJumping)
+                {
+                    playerObject.physComp.velocity.Y = -10.0f;
+                    playerObject.IsJumping = true;
+                    //TODO: Change the Vertical velocity to a higher or lower based on play.
+                }
+                
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+            {
+                
+                playerObject.physComp.velocity.X = -playerObject.MaxHorizontalVelocity;
+                
+            }
+
+
+            if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+            {
+                
+                playerObject.physComp.velocity.X = playerObject.MaxHorizontalVelocity;
+                
+            }
+
+            if ((previousKeyboardState.IsKeyDown(Keys.Left)  && currentKeyboardState.IsKeyUp(Keys.Left)) || (previousGamePadState.DPad.Left == ButtonState.Pressed && currentGamePadState.DPad.Left == ButtonState.Released))
+            {
+                playerObject.physComp.velocity.X = 0;
+            }
+
+            if (previousKeyboardState.IsKeyDown(Keys.Right) && (currentKeyboardState.IsKeyUp(Keys.Right)) || ((previousGamePadState.DPad.Right == ButtonState.Pressed && currentGamePadState.DPad.Right == ButtonState.Released)))
+            {
+                playerObject.physComp.velocity.X = 0;
+            }
+
+
+        }
+
+    }
+}
---
+++ glitch/glitch/PlayerObject.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace glitch
+{
+    public class PlayerObject : GameObject
+    {
+        public float HorizontalAcceleration { get; set; }
+        public float MaxHorizontalVelocity { get; set; }
+        public Boolean IsJumping { get; set; }
+
+        public PlayerObject(Point position, Texture2D sprite, bool isVisible, PhysicsType type) : base(position, sprite, isVisible, type)
+        {
+            setDefaults();
+        }
+
+        public PlayerObject(int x, int y, Texture2D sprite, bool isVisible, PhysicsType type) : base(x, y, sprite, isVisible, type)
+        {
+            setDefaults();
+        }
+
+        /// <summary>
+        /// Set the default values for properties of PlayerObject
+        /// </summary>
+        private void setDefaults()
+        {
+            HorizontalAcceleration = 1.0f;
+            MaxHorizontalVelocity = 0.5f;
+            IsJumping = false;
+        }
+    }
+}
