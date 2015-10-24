4c0f4b23029e8cdfe7abbf8c102f499d13a2ba3f
cpiggott <cpiggott@ksu.edu>
2015-10-24 14:27:23 -0500

    Added more logic for rendering and creating objects for a level

--- glitch/glitch/Level.cs
+++ glitch/glitch/Level.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace glitch
 {
@@ -11,29 +14,53 @@ namespace glitch
 
         public Point PlayerStaringPoint { get; set; }
         public Point DoorPoint { get; set; }
-        public int LevelHeight { get; set; }
+        public int LevelGroundLevel { get; set; }
         public int LevelNumber { get; set; }
+        public GameObject Door { get; set; }
+        public List<GameObject> LevelGroundObjects { get; set; }
 
 
-        public Level(int levelNumber)
+
+        public Level(int levelNumber, Texture2D doorContent)
         {
             this.LevelNumber = levelNumber;
             DefaultValues();
+            LevelGroundObjects = new List<GameObject>();
+            this.Door = new GameObject(DoorPoint, doorContent, true, PhysicsType.StaticObject);
         }
 
-        public Level(int levelNumber, Point playerStartingPoint, Point doorPoint, int levelHeight)
+        public Level(int levelNumber, Point playerStartingPoint, Point doorPoint, int levelHeight, Texture2D doorContent)
         {
             this.LevelNumber = levelNumber;
             this.PlayerStaringPoint = playerStartingPoint;
             this.DoorPoint = doorPoint;
-            this.LevelHeight = levelHeight;
+            this.LevelGroundLevel = levelHeight;
+            LevelGroundObjects = new List<GameObject>();
+
+            this.Door = new GameObject(DoorPoint, doorContent, true, PhysicsType.StaticObject);
         }
 
         private void DefaultValues()
         {
+            LevelGroundObjects = new List<GameObject>();
             this.PlayerStaringPoint = new Point(40, 600);
             this.DoorPoint = new Point(1200, 600); //@TODO: Remove hard coded values
-            this.LevelHeight = 600;
+            this.LevelGroundLevel = 600;
+        }
+
+        public void AddGroundObject(Point groundPoint, Texture2D texture, bool isVisible)
+        {
+            LevelGroundObjects.Add(new GameObject(groundPoint, texture, isVisible, PhysicsType.StaticObject));
+        }
+
+        public void RenderLevel(SpriteBatch spriteBatch)
+        {
+            this.Door.Render(spriteBatch);
+
+            foreach (GameObject gameObject in this.LevelGroundObjects)
+            {
+                gameObject.Render(spriteBatch);
+            }
         }
 
 
