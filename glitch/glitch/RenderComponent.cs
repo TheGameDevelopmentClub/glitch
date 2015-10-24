e0966267c409638345b251a8ccc03e93cac2cfba
Nick Boen <NickABoen@gmail.com>
2015-10-23 22:18:23 -0500

    Adds the outlines for GameObject, RenderComponent, PhysicsComponent, and the Physics System as well as some minor functionality to them.

---
+++ glitch/glitch/RenderComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace glitch
+{
+    class RenderComponent
+    {
+        Texture2D sprite;
+
+        public RenderComponent(Texture2D sprite)
+        {
+            this.sprite = sprite;
+        }
+
+        public void Render(SpriteBatch batch, Vector2 pos)
+        {
+            batch.Draw(this.sprite, pos, Color.White);
+        }
+
+        public void Render(SpriteBatch batch, int x, int y)
+        {
+            this.Render(batch, new Vector2(x, y));
+        }
+
+        public void Render(SpriteBatch batch, Point pos)
+        {
+            this.Render(batch, pos.X, pos.Y);
+        }
+    }
+}
