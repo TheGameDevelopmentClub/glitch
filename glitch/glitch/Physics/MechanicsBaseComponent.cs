c13730fc5158fc7495fb595b501948ae553821ab
cpiggott <cpiggott@ksu.edu>
2015-10-25 11:18:34 -0500

    Fixing issue with merge

---
+++ glitch/glitch/Physics/MechanicsBaseComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace glitch.Physics
+{
+    public abstract class MechanicsBaseComponent : PhysicsComponent
+    {
+        public MechanicsBaseComponent(int width, int height, PhysicsType type):base(width, height, type) { }
+
+        public abstract bool StopPlayerMovement { get; }
+
+        /// <summary>
+        /// The mechanic doesn't necessarily have affect the player directly.
+        /// The player is passed in here as a convenience
+        /// </summary>
+        /// <param name="player">Convenience reference to the Mechanic Method</param>
+        public abstract void ApplyMechanic(PlayerObject player);
+    }
+}
